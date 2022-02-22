cbab3da8e29ced591f963489523c98dbb30f1cb6
Anderson S. Vieira <svieira.anderson@gmail.com>
2022-02-22 10:27:57 -0300

    começa o código e cria um quadrado

--- cap_04_projeto_interface/meu_poligono.py
+++ cap_04_projeto_interface/meu_poligono.py
@@ -1,1 +1,18 @@
+
 #!/usr/bin/env python3
+
+import turtle
+
+bob = turtle.Turtle()
+
+bob.fd(100)
+bob.lt(90)
+bob.fd(100)
+bob.lt(90)
+bob.fd(100)
+bob.lt(90)
+bob.fd(100)
+
+# print(bob)
+
+turtle.mainloop()

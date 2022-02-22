2c9b743484713a43b524e96f3abbc7cf2db682b1
Anderson S. Vieira <svieira.anderson@gmail.com>
2022-02-22 14:02:14 -0300

    cria circle()

--- cap_04_projeto_interface/meu_poligono.py
+++ cap_04_projeto_interface/meu_poligono.py
@@ -1,6 +1,7 @@
 
 #!/usr/bin/env python3
 
+import math
 import turtle
 
 # Definições das funções
@@ -11,18 +12,24 @@ def square(x, comprimento):
 
 def polygon(x, n, comprimento):
     angulo = 360 / n
+    # print(comprimento*n)
     for i in range(n):
         x.fd(comprimento)
         x.lt(angulo)
 
+def circle(x, r):
+    circunferencia = 2 * math.pi * r
+    # print(circunferencia)
+    n = 50
+    comprimento = circunferencia /n
+    polygon(x, n, comprimento)
 
 # Cria 'bob'.
 bob = turtle.Turtle()
 
-
 # Chama a função
 # square(bob, 150)
-
-polygon(bob, 5, 150)
+# polygon(bob, 5, 150)
+circle(bob, 50)
 
 turtle.mainloop()

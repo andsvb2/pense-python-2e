8d4a0455fe544c6edfe10d11132519b7c502ee45
Anderson S. Vieira <svieira.anderson@gmail.com>
2022-02-22 10:35:17 -0300

    transforma as instruções num laço for

--- cap_04_projeto_interface/meu_poligono.py
+++ cap_04_projeto_interface/meu_poligono.py
@@ -5,13 +5,17 @@ import turtle
 
 bob = turtle.Turtle()
 
-bob.fd(100)
-bob.lt(90)
-bob.fd(100)
-bob.lt(90)
-bob.fd(100)
-bob.lt(90)
-bob.fd(100)
+# bob.fd(100)
+# bob.lt(90)
+# bob.fd(100)
+# bob.lt(90)
+# bob.fd(100)
+# bob.lt(90)
+# bob.fd(100)
+
+for i in range(4):
+    bob.fd(100)
+    bob.lt(90)
 
 # print(bob)
 
